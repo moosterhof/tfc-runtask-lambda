0ff704d3e997e2f0db874201d6e7ea9e64c826e4
Michel Oosterhof <moosterhof@hashicorp.com>
2021-09-30 19:45:28 +0800

    working lambda that logs

--- src/opa/go.mod
+++ src/opa/go.mod
@@ -4,6 +4,5 @@ go 1.16
 
 require (
 	github.com/aws/aws-lambda-go v1.27.0
-	github.com/stretchr/testify v1.7.0 // indirect
-	gopkg.in/yaml.v3 v3.0.0-20210107192922-496545a6307b // indirect
+	github.com/open-policy-agent/opa v0.32.1
 )
--- src/opa/handler/handler.go
+++ src/opa/handler/handler.go
@@ -1,20 +1,39 @@
 package handler
 
 import (
+	"bytes"
 	"context"
 	"encoding/json"
-
+//	"errors"
+	"fmt"
 	"github.com/aws/aws-lambda-go/events"
+//	"github.com/aws/aws-lambda-go/lambda"
+//        "github.com/aws/aws-lambda-go/lambdacontext"
+	"github.com/open-policy-agent/opa/ast"
+//	"github.com/open-policy-agent/opa/loader"
+//	"github.com/open-policy-agent/opa/rego"
+	"github.com/open-policy-agent/opa/storage"
+	"io/ioutil"
+	"log"
+	"net/http"
+//	"time"
+)
+
+var (
+        err      error
+        compiler *ast.Compiler
+        store    storage.Store
+        ctx      = context.Background()
 )
 
 type Response events.APIGatewayProxyResponse
 
 // Handler - interface
 type Handler interface {
-	Run(ctx context.Context, event events.APIGatewayCustomAuthorizerRequest) (Response, error)
+	Run(ctx context.Context, event events.APIGatewayProxyRequest) (Response, error)
 }
 
-type lambdaHander struct {
+type lambdaHandler struct {
 	hmacKey string
 }
 
@@ -22,10 +41,12 @@ type LambdaResponse struct {
 	Message string
 }
 
-func (l lambdaHander) Run(ctx context.Context, event events.APIGatewayCustomAuthorizerRequest) (Response, error) {
+func (l lambdaHandler) Run(ctx context.Context, event events.APIGatewayProxyRequest) (Response, error) {
+
 	lambdaResponse := LambdaResponse{
-		Message: "Hello " + l.hmacKey,
+		Message: "Hello " + fmt.Sprint(event),
 	}
+        log.Print("Request body: ", event)
 
 	response, err := json.Marshal(lambdaResponse)
 
@@ -49,11 +70,91 @@ func (l lambdaHander) Run(ctx context.Context, event events.APIGatewayCustomAuth
 	return res, err
 }
 
+func init() {
+/*
+        policyData, err := loader.All([]string{"data"})
+        if err != nil {
+                log.Fatalf("Failed to load bundle from disk: %v", err)
+        }
+
+        // Compile the module. The keys are used as identifiers in error messages.
+        compiler, err = policyData.Compiler()
+        if err != nil {
+                log.Fatalf("Failed to compile policies in bundle: %v", err)
+        }
+
+        store, err = policyData.Store()
+        if err != nil {
+                log.Fatalf("Failed to create storage from bundle: %v", err)
+        }
+*/
+}
+
+// This GET's additional information from TFC
+func TFCget(url string) {
+
+	resp, err := http.Get(url)
+        // TODO: need token here?
+	// resp.Header.Set("Authorization", "Bearer "+token)
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	defer resp.Body.Close()
+
+	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	log.Println(string(body))
+
+	return
+}
+
+// This PATCH'es the output back to TFC
+func tfcCallback(message string, url string, token string) {
+
+	payload, err := json.Marshal(message)
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	/*
+	// payload needs to look like this:
+        // status can be `passed` or `failed`
+	{
+	  "data": {
+	    "type": "task-results",
+	      "attributes": {
+	        "status": "passed",
+	        "message": "Hello task"
+	      }
+	  }
+	}
+	*/
+
+	resp, err := http.NewRequest("PATCH", url, bytes.NewBuffer(payload))
+	resp.Header.Set("Content-Type", "application/json")
+	resp.Header.Set("Authorization", "Bearer "+token)
+
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	var result map[string]interface{}
+
+	json.NewDecoder(resp.Body).Decode(&result)
+
+	log.Println(result)
+	log.Println(result["data"])
+}
+
 // NewLambdaHandler -
 func NewLambdaHandler(
 	hmacKey string,
-) *lambdaHander {
-	return &lambdaHander{
+) *lambdaHandler {
+	return &lambdaHandler{
 		hmacKey: hmacKey,
 	}
 }
