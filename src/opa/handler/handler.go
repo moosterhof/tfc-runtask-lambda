6a6c25333ca05567b62fa0106cfb8579595fe39f
Michel Oosterhof <moosterhof@hashicorp.com>
2021-10-01 17:58:15 +0800

    better

--- src/opa/handler/handler.go
+++ src/opa/handler/handler.go
@@ -44,25 +44,25 @@ type LambdaResponse struct {
 }
 
 type RequestBody struct {
-	Payload_version               float64  `json:"payload_version"`
-	Access_token                  string   `json:"access_token"`
-	Task_result_id                string   `json:"task_result_id"`
-	Task_result_enforcement_level string   `json:"task_result_enforcement_level"`
-	Task_result_callback_url      string   `json:"task_result_callback_url"`
-	Run_app_url                   string   `json:"run_app_url"`
-	Run_id                        string   `json:"run_id"`
-	Run_message                   string   `json:"run_message"`
-	Run_created_at                string   `json:"run_created_at"`
-	Run_created_by                string   `json:"run_created_by"`
-	Workspace_id                  string   `json:"workspace_id"`
-	Workspace_name                string   `json:"workspace_name"`
-	Workspace_app_url             string   `json:"workspace_app_url"`
-	Organization_name             string   `json:"organization_name"`
-	Plan_json_api_url             string   `json:"plan_json_api_url"`
-	Vcs_repo_url                  string   `json:"vcs_repo_url"`
-	Vcs_branch                    string   `json:"vcs_branch"`
-	Vcs_pull_request_url          string   `json:"vcs_pull_request_url"`
-	Vcs_commit_url                string   `json:"vcs_commit_url"`
+	Payload_version               float64 `json:"payload_version"`
+	Access_token                  string  `json:"access_token"`
+	Task_result_id                string  `json:"task_result_id"`
+	Task_result_enforcement_level string  `json:"task_result_enforcement_level"`
+	Task_result_callback_url      string  `json:"task_result_callback_url"`
+	Run_app_url                   string  `json:"run_app_url"`
+	Run_id                        string  `json:"run_id"`
+	Run_message                   string  `json:"run_message"`
+	Run_created_at                string  `json:"run_created_at"`
+	Run_created_by                string  `json:"run_created_by"`
+	Workspace_id                  string  `json:"workspace_id"`
+	Workspace_name                string  `json:"workspace_name"`
+	Workspace_app_url             string  `json:"workspace_app_url"`
+	Organization_name             string  `json:"organization_name"`
+	Plan_json_api_url             string  `json:"plan_json_api_url"`
+	Vcs_repo_url                  string  `json:"vcs_repo_url"`
+	Vcs_branch                    string  `json:"vcs_branch"`
+	Vcs_pull_request_url          string  `json:"vcs_pull_request_url"`
+	Vcs_commit_url                string  `json:"vcs_commit_url"`
 }
 
 func (l lambdaHandler) Run(ctx context.Context, request events.APIGatewayProxyRequest) (Response, error) {
@@ -75,6 +75,7 @@ func (l lambdaHandler) Run(ctx context.Context, request events.APIGatewayProxyRe
 	// case is like: X-Tfc-Event-Hook-Signature
 	signature := request.Headers["X-Tfc-Event-Hook-Signature"]
 	log.Print("HMAC header: ###", signature, "###")
+	log.Print("HMAC key: ###", l.hmacKey, "###")
 
 	mac := hmac.New(sha512.New, []byte(l.hmacKey))
 	mac.Write([]byte(request.Body))
@@ -127,17 +128,17 @@ func (l lambdaHandler) Run(ctx context.Context, request events.APIGatewayProxyRe
 
 	if r.Access_token == "test-token" {
 		log.Print("Detected new run task registration through test request")
-                // NO CHECK IS EXECUTED
+		// NO CHECK IS EXECUTED
 		return buildResponse(LambdaResponse{Message: "Test Request Accepted"})
 	}
 
-        // ACTUAL CHECK HAPPENS HERE
+	// ACTUAL CHECK HAPPENS HERE
 
-        passed := true
-        message := "testing 1 2 3"
-        tfcCallback(message, passed, r.Task_result_callback_url, r.Access_token)
+	passed := true
+	message := "testing 1 2 3"
+	tfcCallback(message, passed, r.Task_result_callback_url, r.Access_token)
 
-        return buildResponse(LambdaResponse{Message: "Request Accepted"})
+	return buildResponse(LambdaResponse{Message: "Request Accepted"})
 }
 
 func buildResponse(lambdaResponse LambdaResponse) (Response, error) {
@@ -204,57 +205,40 @@ func TFCget(url string) {
 }
 
 // This PATCH'es the output back to TFC
-/*
-		// payload needs to look like this:
-	        // status can be `passed` or `failed`
-		{
-		  "data": {
-		    "type": "task-results",
-		      "attributes": {
-		        "status": "passed",
-		        "message": "Hello task"
-		      }
-		  }
-		}
-*/
 func tfcCallback(message string, pass bool, url string, token string) {
-
-	//payload, err := json.Marshal(message)
-	//if err != nil {
-	//	log.Fatalln(err)
-	//}
-
 	var status string
 	if pass {
 		status = "passed"
 	} else {
 		status = "failed"
 	}
+	/*
+			// payload needs to look like this:
+		        // status can be `passed` or `failed`
+			{
+			  "data": {
+			    "type": "task-results",
+			      "attributes": {
+			        "status": "passed",
+			        "message": "Hello task"
+			      }
+			  }
+			}
+	*/
 
 	payload := "{ \"data\": { \"type\": \"task-results\", \"attributes\": { \"status\": \"" + status + "\", \"message\": \"" + string(message) + "\"} } }"
 
-        log.Print("Payload: ###", payload, "###")
-        log.Print("URL: ###", url, "###")
-        log.Print("Token: ###", token, "###")
-
-        client := &http.Client{}
+	client := &http.Client{}
 	req, err := http.NewRequest("PATCH", url, strings.NewReader(payload))
 	req.Header.Set("Content-Type", "application/vnd.api+json")
 	req.Header.Set("Authorization", "Bearer "+token)
 
-        log.Print("request: ###", req, "###")
+	log.Print("request: ###", req, "###")
 
-        resp, err := client.Do(req)
+	resp, err := client.Do(req)
 	if err != nil {
 		log.Fatalln(err)
 	}
-
-	var result map[string]interface{}
-
-	json.NewDecoder(resp.Body).Decode(&result)
-
-	log.Print(result)
-	log.Print(result["data"])
 }
 
 // NewLambdaHandler -
