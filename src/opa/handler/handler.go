7b01cfafb49a75572707f0a293e3bb4f3ed37876
Michel Oosterhof <moosterhof@hashicorp.com>
2021-10-01 15:13:43 +0800

    new way to parse

--- src/opa/handler/handler.go
+++ src/opa/handler/handler.go
@@ -1,13 +1,18 @@
 package handler
 
 import (
-	"bytes"
 	"context"
 	"crypto/hmac"
 	"crypto/sha512"
 	"encoding/json"
 	//	"errors"
 	"fmt"
+	"io/ioutil"
+	"log"
+	"net/http"
+	"strings"
+	//	"time"
+
 	"github.com/aws/aws-lambda-go/events"
 	//	"github.com/aws/aws-lambda-go/lambda"
 	//        "github.com/aws/aws-lambda-go/lambdacontext"
@@ -15,11 +20,6 @@ import (
 	//	"github.com/open-policy-agent/opa/loader"
 	//	"github.com/open-policy-agent/opa/rego"
 	"github.com/open-policy-agent/opa/storage"
-	"io/ioutil"
-	"log"
-	"net/http"
-	"strings"
-	//	"time"
 )
 
 var (
@@ -45,48 +45,37 @@ type LambdaResponse struct {
 }
 
 type TFCRunTask struct {
-	access_token             string
-	task_result_callback_url string
-	plan_json_api_url        string
+	payload_version               int
+	access_token                  string
+	task_result_id                string
+	task_result_enforcement_level string
+	task_result_callback_url      string
+	run_app_url                   string
+	run_id                        string
+	run_message                   string
+	run_created_at                string
+	run_created_by                string
+	workspace_id                  string
+	workspace_name                string
+	workspace_app_url             string
+	organization_name             string
+	plan_json_api_url             string
+	vcs_repo_url                  string
+	vcs_branch                    string
+	vcs_pull_request_url          string
+	vcs_commit_url                string
 }
 
 func (l lambdaHandler) Run(ctx context.Context, request events.APIGatewayProxyRequest) (Response, error) {
 
-	log.Print("Request body: ", request.Body)
-	log.Print("Request headers: ", request.Headers)
-
-	/*
-				we can get 2 types of POSTs, a test POST to test the handler, should return 200
-				and a full test, both should return 200 or and one off 'pass/fail'
-		                the test request does not have a HMAC signature header
-				the test request body looks like this:
-				   {
-				       "payload_version": 1,
-				       "access_token": "test-token",
-				       "task_result_id": "taskrs-xxxxxxxxxxxxxxxx",
-				       "task_result_enforcement_level": "test",
-				       "task_result_callback_url": "https://app.terraform.io/api/v2/task-results/xxxxxxxx-xxxx-xxxx-xxxx-xxxxxxxxxxxx/callback",
-				       "run_app_url": "https://app.terraform.io/app/test-org/test-workspace/runs/run-xxxxxxxxxxxxxxxx",
-				       "run_id": "run-xxxxxxxxxxxxxxxx",
-				       "run_message": "Test run message",
-				       "run_created_at": "2021-01-01T00:00:00.000Z",
-				       "run_created_by": "test-user",
-				       "workspace_id": "ws-xxxxxxxxxxxxxxxx",
-				       "workspace_name": "test-workspace",
-				       "workspace_app_url": "https://app.terraform.io/app/test-org/test-workspace",
-				       "organization_name": "test-org",
-				       "plan_json_api_url": "https://app.terraform.io/api/v2/plans/plan-xxxxxxxxxxxxxxxx/json-output",
-				       "vcs_repo_url": "https://github.com/test-org/test-repo",
-				       "vcs_branch": "main",
-				       "vcs_pull_request_url": "https://github.com/test-org/test-repo/pull/1",
-				       "vcs_commit_url": "https://github.com/test-org/test-repo/commit/1234567sha"
-				   }
-	*/
+	log.Print("OPA Lambda starting", request.Body)
+	log.Print("Request body: ###", request.Body, "###")
+	log.Print("Request headers: ###", request.Headers, "###")
 
 	// this is the HMAC for the request, if available
 	// case is like: X-Tfc-Event-Hook-Signature
 	signature := request.Headers["X-Tfc-Event-Hook-Signature"]
-	log.Print("HMAC header: ", signature)
+	log.Print("HMAC header: ###", signature, "###")
 
 	mac := hmac.New(sha512.New, []byte(l.hmacKey))
 	mac.Write([]byte(request.Body))
@@ -96,24 +85,51 @@ func (l lambdaHandler) Run(ctx context.Context, request events.APIGatewayProxyRe
 	if match {
 		log.Print("VALID MAC")
 	} else {
-		log.Print("WRONG MAC")
+		log.Print("INCORRECT MAC")
 	}
 
-	// verify HMAC here
-	//verify := verifyhmac(request.Body, hmac, hmacKey)
-	// if error, return 500
+	/*
+		we can get 2 types of POSTs, a test POST to test the handler, should return 200
+		and a full test, both should return 200 or and one of 'pass/fail'
+		the test request body looks like this:
+		   {
+		       "payload_version": 1,
+		       "access_token": "test-token",
+		       "task_result_id": "taskrs-xxxxxxxxxxxxxxxx",
+		       "task_result_enforcement_level": "test",
+		       "task_result_callback_url": "https://app.terraform.io/api/v2/task-results/xxxxxxxx-xxxx-xxxx-xxxx-xxxxxxxxxxxx/callback",
+		       "run_app_url": "https://app.terraform.io/app/test-org/test-workspace/runs/run-xxxxxxxxxxxxxxxx",
+		       "run_id": "run-xxxxxxxxxxxxxxxx",
+		       "run_message": "Test run message",
+		       "run_created_at": "2021-01-01T00:00:00.000Z",
+		       "run_created_by": "test-user",
+		       "workspace_id": "ws-xxxxxxxxxxxxxxxx",
+		       "workspace_name": "test-workspace",
+		       "workspace_app_url": "https://app.terraform.io/app/test-org/test-workspace",
+		       "organization_name": "test-org",
+		       "plan_json_api_url": "https://app.terraform.io/api/v2/plans/plan-xxxxxxxxxxxxxxxx/json-output",
+		       "vcs_repo_url": "https://github.com/test-org/test-repo",
+		       "vcs_branch": "main",
+		       "vcs_pull_request_url": "https://github.com/test-org/test-repo/pull/1",
+		       "vcs_commit_url": "https://github.com/test-org/test-repo/commit/1234567sha"
+		   }
+	*/
 
-	dec := json.NewDecoder(strings.NewReader(request.Body))
-	//dec.DisallowUnknownFields()
 	var r TFCRunTask
-	err := dec.Decode(&r)
+	err = json.Unmarshal([]byte(request.Body), &r)
 	if err != nil {
 		log.Print("json decoding error: ", err)
 	}
 
+	log.Print("access token: " + r.access_token)
+	log.Print("task_result_callback_url: " + r.task_result_callback_url)
+	log.Print("plan_json_api_url: " + r.plan_json_api_url)
+
 	if r.access_token == "test-token" {
 		log.Print("Detected new run task registration through test request")
 		return buildResponse(LambdaResponse{Message: "Test Request Accepted"})
+	} else {
+		log.Print("normal task accepted")
 	}
 
 	lambdaResponse := LambdaResponse{
@@ -205,28 +221,36 @@ func TFCget(url string) {
 }
 
 // This PATCH'es the output back to TFC
-func tfcCallback(message string, url string, token string) {
+/*
+		// payload needs to look like this:
+	        // status can be `passed` or `failed`
+		{
+		  "data": {
+		    "type": "task-results",
+		      "attributes": {
+		        "status": "passed",
+		        "message": "Hello task"
+		      }
+		  }
+		}
+*/
+func tfcCallback(message string, pass bool, url string, token string) {
 
-	payload, err := json.Marshal(message)
-	if err != nil {
-		log.Fatalln(err)
+	//payload, err := json.Marshal(message)
+	//if err != nil {
+	//	log.Fatalln(err)
+	//}
+
+	var status string
+	if pass {
+		status = "passed"
+	} else {
+		status = "failed"
 	}
 
-	/*
-			// payload needs to look like this:
-		        // status can be `passed` or `failed`
-			{
-			  "data": {
-			    "type": "task-results",
-			      "attributes": {
-			        "status": "passed",
-			        "message": "Hello task"
-			      }
-			  }
-			}
-	*/
+	payload := "{ \"data\": { \"type\": \"task-results\", \"attributes\": { \"status\": \"" + status + "\", \"message\": \"" + string(message) + "\"} } }"
 
-	resp, err := http.NewRequest("PATCH", url, bytes.NewBuffer(payload))
+	resp, err := http.NewRequest("PATCH", url, strings.NewReader(payload))
 	resp.Header.Set("Content-Type", "application/json")
 	resp.Header.Set("Authorization", "Bearer "+token)
 
